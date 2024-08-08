117e7e168312f07a3ec8fdea3f722b79805f341e
vipul <vipul01232000@gmail.com>
2024-08-08 13:27:26 +0530

    Initial commit of MFCApplication2 project

---
+++ MFCApplication2Dlg.cpp
@@ -0,0 +1,130 @@
+
+// MFCApplication2Dlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication2.h"
+#include "MFCApplication2Dlg.h"
+#include "afxdialogex.h"
+#include <fstream>
+#include <string>
+using namespace std;
+
+#pragma comment(lib, "Shlwapi.lib")
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication2Dlg dialog
+
+
+
+CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication2Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CMFCApplication2Dlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication2Dlg message handlers
+
+BOOL CMFCApplication2Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication2Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCApplication2Dlg::OnBnClickedOk()
+{
+	
+
+	WriteLog(L"This is a log message.");
+
+	CDialogEx::OnOK();
+
+}
+
+void CMFCApplication2Dlg::WriteLog(CString logMessage)
+{
+	// Ensure the log folder exists using Windows API
+	CString logFolder = L"C:\\logs";
+	if (CreateDirectory(logFolder, NULL) != ERROR_SUCCESS  ) 
+	{
+		AfxMessageBox(_T("Unable to create log folder: ") + logFolder);
+		return;
+	}
+
+	// Construct the full path for the log file
+	CString logFilePath = logFolder + "\\" + L"FileExplorer.log";
+
+	// Open the log file in append mode
+	std::wofstream logFile((LPCTSTR)logFilePath, std::wofstream::app);
+	if (logFile.is_open()) 
+	{
+		// Write the log message
+		logFile << (LPCTSTR)logMessage << std::endl;
+		logFile.close();
+	}
+	else {
+		AfxMessageBox(_T("Unable to open log file: ") + logFilePath);
+	}
+}
